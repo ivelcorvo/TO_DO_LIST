9d4d791e8324eaea050878bfab6c91c21d260539
ivelcorvo <levialvesjr@hotmail.com>
2025-09-19 09:38:06 -0300

    alterações

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <footer className=" px-3 pt-5 flex flex-col justify-center items-center">
-      <div className="mx-auto md:mx-0 mt-5 md:mt-0" >
-        <a href="https://www.linkedin.com/in/levi-alves-junior-09b91a189/" target="_blank" rel="noreferrer" className="m-2">
+      <div className="mx-auto md:mx-0 mt-5 md:mt-0 flex" >
+        <a href="https://www.linkedin.com/in/levi-alves-junior-09b91a189/" target="_blank" rel="noreferrer" className="m-2 hover:scale-150 block">
           <i className="text-gray-600 fa-brands fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://github.com/ivelcorvo" target="_blank" rel="noreferrer" className="m-2">
+        <a href="https://github.com/ivelcorvo" target="_blank" rel="noreferrer" className="m-2 hover:scale-150 block">
           <i className="text-gray-600 fa-brands fa-square-github fa-2x"></i>
         </a>
       </div>
